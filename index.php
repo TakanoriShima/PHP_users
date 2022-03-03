1a955096bd182378267fd5563ccf61bd8ec37caf
EC2 Default User <ec2-user@ip-172-31-9-174.ap-northeast-1.compute.internal>
2022-03-03 12:05:49 +0900

    MySQLからデータを取得して表示

--- index.php
+++ index.php
@@ -5,26 +5,29 @@
     // セッション開始
     session_start();
     
-    $users = $_SESSION['users'];
+    // $users = $_SESSION['users'];
     $flush = $_SESSION['flush'];
     $_SESSION['flush'] = null;
+    
+    // Userモデルを使って、MySQLから全ユーザー情報を取得
+    $users = User::all();
     // セッションにユーザー一覧が保存されていなければ
-    if($users === null){
-        // 物語開始
-        $iwai = new User('岩井', 25, 'male');
-        $shima = new User('島', 49, 'male');
-        $yamada = new User('山田', 10, 'female');
+    // if($users === null){
+    //     // 物語開始
+    //     $iwai = new User('岩井', 25, 'male');
+    //     $shima = new User('島', 49, 'male');
+    //     $yamada = new User('山田', 10, 'female');
         
-        // ユーザー一覧作成
-        $users = array(); // let users = Array();
-        // $users = [];
-        $users[] = $iwai;
-        // array_push($users, $iwai); // users.push(iwai);
-        $users[] = $shima;
-        $users[] = $yamada;
-        // セッションにユーザー一覧を保存
-        $_SESSION['users'] = $users;
-    }
+    //     // ユーザー一覧作成
+    //     $users = array(); // let users = Array();
+    //     // $users = [];
+    //     $users[] = $iwai;
+    //     // array_push($users, $iwai); // users.push(iwai);
+    //     $users[] = $shima;
+    //     $users[] = $yamada;
+    //     // セッションにユーザー一覧を保存
+    //     $_SESSION['users'] = $users;
+    // }
     // var_dump($users);
     // print_r($users);
     // ユーザー一覧表示
--- models/User.php
+++ models/User.php
@@ -1,13 +1,16 @@
 <?php
+    require_once 'models/Model.php';
     // モデル(M)
     // ユーザーの設計図
-    class User {
+    class User extends Model{
         // プロパティ
+        public $id; // ID
         public $name; // 名前
         public $age; // 年齢
         public $gender; // 性別
+        public $created_at; // 登録日時
         // コンストラクタ
-        public function __construct($name, $age, $gender){
+        public function __construct($name='', $age='', $gender=''){
             // プロパティの初期化
             $this->name = $name;
             $this->age = $age;
@@ -34,5 +37,21 @@
                 $errors[] = '年齢は正の整数を入力してください';
             }
             return $errors;
+        }
+        
+        // 全テーブル情報を取得するメソッド
+        public static function all(){
+            try {
+                $pdo = self::get_connection();
+                $stmt = $pdo->query('SELECT * FROM users ORDER BY id DESC');
+                // フェッチの結果を、Userクラスのインスタンスにマッピングする
+                $stmt->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'User');
+                $users = $stmt->fetchAll();
+                self::close_connection($pdo, $stmt);
+                // Userクラスのインスタンスの配列を返す
+                return $users;
+            } catch (PDOException $e) {
+                return 'PDO exception: ' . $e->getMessage();
+            }
         }
     }
